b67967373f22d1549c8b876054a19e9a98510eec
Sam Burnett <srburnet@gmail.com>
2011-12-15 04:26:15 +0000

    Move uploader modules to versioned code

--- data_server/views.py
+++ data_server/views.py
@@ -8,12 +8,22 @@ from django.http import HttpResponse
 
 node_id_matcher = re.compile(r'OW[0-9A-F]{12}$')
 
+uploader_modules = {
+    'active': None,
+    'bismark-updater': 10 * 2**20,
+    'nazanin-traceroute': 10 * 2**20,
+    'passive': 10 * 2**20,
+    'passive-frequent': 100 * 2**10,
+    'wifi-beacons': 10 * 2**20,
+}
+
 def upload(request):
     module = request.REQUEST['directory']
-    if module not in settings.UPLOADER_MODULES:
+    if module not in uploader_modules:
         raise ValueError('Invalid module')
-    if len(request.raw_post_data) > settings.UPLOADER_MODULES[module]:
-        raise ValueError('Upload is too big')
+    if uploader_modules[module] is not None \
+            and len(request.raw_post_data) > uploader_modules[module]:
+                raise ValueError('Upload is too big')
     if node_id_matcher.match(request.REQUEST['node_id']) is None:
         raise ValueError('Invalid node id')
 
